95f5e0a0e700e911c3c82977aa671f27f3cf927d
Rob Cakebread <cakebread@gmail.com>
2007-04-20 17:47:32 +0000

    Change to GPL-2 license

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from yolk import __version__
 
 
 setup(name="yolk",
-    license = "PSF",
+    license = "GPL-2",
     version=__version__.VERSION,
     description="Library and command-line tool for listing packages installed by setuptools, their metadata and dependencies and PyPI querying.",
     long_description=open("README", "r").read(),
@@ -17,7 +17,7 @@ setup(name="yolk",
     keywords="PyPI setuptools cheeseshop distutils eggs package management",
     classifiers=["Development Status :: 2 - Pre-Alpha",
                  "Intended Audience :: Developers",
-                 "License :: OSI Approved :: Python Software Foundation License",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
                  "Programming Language :: Python",
                  "Topic :: Software Development :: Libraries :: Python Modules",
                  ],
