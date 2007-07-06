a96f7fee01cea8839d29779652738db0723fdfdd
Rob Cakebread <cakebread@gmail.com>
2007-07-06 21:07:31 +0000

    Add install_reqires and celementree-foo

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python
 
+import sys
+
 from setuptools import setup
 
 from yolk import __version__
 
+
+#ElementTree does not use setuptools in its setup.py. Because virtually every
+#distro except Windows has an elementree package available already in their
+#package system, we see if its installed and if so, don't add it to 
+#install_requires, avoiding double-installation because setuptools can't find
+#a version of it installed by distutils.
+
+
+install_requires=["setuptools"]
+
+#Python >=2.5 has elementtree 
+if sys.version_info[0] == 2 and sys.version_info[1] == 5:
+    #cElementTree is in stdlib, safe to add to install_requires
+    install_requires.append("cElementTree")
+else:
+    #For Python <=2.4
+    try:
+        from elementtree.ElementTree import parse
+    except ImportError:
+        install_requires.append("cElementTree")
 
 setup(name="yolk",
     license = "GPL-2",
@@ -21,6 +43,8 @@ setup(name="yolk",
                  "Programming Language :: Python",
                  "Topic :: Software Development :: Libraries :: Python Modules",
                  ],
+    install_requires=install_requires,
+    tests_require=["nose"],
     packages=['yolk', 'yolk.plugins'],
     package_dir={'yolk':'yolk'},
     entry_points={'console_scripts': ['yolk = yolk.cli:main',]},
